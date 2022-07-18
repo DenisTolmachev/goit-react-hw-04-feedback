b998c84f4890e3396e0b3eca2cde8afa08c96bb3
Denis <denis.tolmachev@gmail.com>
2022-07-18 21:24:36 +0300

    correct style

--- src/components/FeedbackOptions/FeedbackOptions.styled.js
+++ src/components/FeedbackOptions/FeedbackOptions.styled.js
@@ -9,7 +9,9 @@ export const Button = styled.button`
   color: ${props => props.theme.colors.white};
   border: none;
   border-radius: 5px;
-  box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.75);
+  transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1) 0s;
+  box-shadow: rgb(0 0 0 / 20%) 0px 3px 1px -2px,
+    rgb(0 0 0 / 14%) 0px 2px 2px 0px, rgb(0 0 0 / 12%) 0px 1px 5px 0px;
   cursor: pointer;
   :hover {
     background-color: ${props => props.theme.colors.secondary};
